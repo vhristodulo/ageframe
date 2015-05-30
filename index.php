3bd00cbfa93baf54f9d6347d599113ad9249f4a2
vhristodulo <vhristodulo@localhost>
2015-05-30 13:06:49 +0200

    front controller (config, router, loader)

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
      * Define base path
      */
     $base_path = 'http://localhost/projects/ageframe/';
-    //echo 'Base path: '.$base_path.'<br/><br/>';
 
     /**
      * Define default controller, action and parameters
@@ -32,41 +31,22 @@
      */
 
     /**
-     * Get URL protocol
+     * Get URL protocol, server name and requested url path
      */
     $url_protocol = filter_input(INPUT_SERVER, 'REQUEST_SCHEME');
-    //echo 'URL protocol: '.$url_protocol.'<br/>';
-
-    /**
-     * Get server name
-     */
     $server_name = filter_input(INPUT_SERVER, 'SERVER_NAME');
-    //echo 'Server name: '.$server_name.'<br/>';
-
-    /**
-     * Get requested URL
-     */
     $requested_url = filter_input(INPUT_SERVER, 'REQUEST_URI');
-    //echo 'Requested URL: '.$requested_url.'<br/><br/>';
 
     /**
      * Generate full path
      */
     $full_path = $url_protocol.'://'.$server_name.$requested_url;
-    //echo 'Full path: '.$full_path.'<br/><br/>';
 
     /**
-     * Remove base path from full path
+     * Remove base path from full path and split rest to array
      */
     $path = str_replace($base_path, '', $full_path);
-    //echo 'Path: '.$path.'<br/>';
-
-    /**
-     * Split path into array
-     */
     $url = explode('/', $path);
-    //print_r($url);
-    //echo '<br/><br/>';
 
     /**
      * Determine controller, action and parameters from array
