df3fd0ce32bcd391eddfdb388245d6aea09d512f
vhristodulo <vhristodulo@localhost>
2015-05-30 16:43:55 +0200

    error controller

--- app/controllers/error.php
+++ app/controllers/error.php
@@ -1,8 +1,10 @@
 <?php
 
+    require_once './core/controllers/base.php';
+
     function error() {
-        global $views_path;
-        require_once $views_path.'error.php';
+        $data['title'] = 'Error Page';
+        load_view('error', $data);
     }
 
 /* End Of File > ./app/controllers/error.php */
