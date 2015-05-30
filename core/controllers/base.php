bcf4f0147ad6ae58580d998fc6bcf73ce6b00db9
vhristodulo <vhristodulo@localhost>
2015-05-30 16:56:23 +0200

    base controller

--- core/controllers/base.php
+++ core/controllers/base.php
@@ -5,5 +5,11 @@
         foreach($data as $key => $value) ${$key} = $value;
         require_once $views_path.$view.'.php';
     }
+    
+    function load_model($model, $data = array()) {
+        global $models_path;
+        foreach($data as $key => $value) ${$key} = $value;
+        require_once $models_path.$model.'.php';
+    }
 
 /* End Of File > ./core/controllers/base.php */
