cf616c484abbdd0b24f8a0e36de86db0cc8558f3
vhristodulo <vhristodulo@localhost>
2015-05-31 04:17:43 +0200

    Base Controller

--- core/controllers/base.php
+++ core/controllers/base.php
@@ -1,11 +1,30 @@
 <?php
 
+    /**
+     * Base Controller
+     * 
+     */
+
+    /**
+     * View loader function
+     * 
+     * @global string $views_path
+     * @param string $view
+     * @param array $data
+     */
     function load_view($view, $data = array()) {
         global $views_path;
         foreach($data as $key => $value) ${$key} = $value;
         require_once $views_path.$view.'.php';
     }
     
+    /**
+     * Model loader function
+     * 
+     * @global string $models_path
+     * @param string $model
+     * @param array $data
+     */
     function load_model($model, $data = array()) {
         global $models_path;
         foreach($data as $key => $value) ${$key} = $value;
