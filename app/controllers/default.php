2b92e4102939bb8e6e00944a62504d099380cf47
vhristodulo <vhristodulo@localhost>
2015-05-31 04:16:54 +0200

    Default Controller

--- app/controllers/default.php
+++ app/controllers/default.php
@@ -1,7 +1,18 @@
 <?php
 
+    /**
+     * Default Controller
+     * 
+     */
+
+    /**
+     * Include Base Controller
+     */
     require_once './core/controllers/base.php';
 
+    /**
+     * Default action
+     */
     function index() {
         load_model('data');
         $data = getData();
