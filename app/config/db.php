3a2e50fd74a9a4a0b32dd7877d634a29437dc1fd
vhristodulo <vhristodulo@localhost>
2015-05-31 05:17:50 +0200

    Database Configuration

---
+++ app/config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * Database configuration file
+     * 
+     */
+
+    /**
+     * Define database connection parameters
+     */
+    $db_hostname = 'localhost';
+    $db_username = 'root';
+    $db_password = 'root';
+    $db_database = 'test';
+
+/* End Of File > ./app/config/db.php */
