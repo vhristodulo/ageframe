be802792fc3d9cdd7b75a92446aac1b3397f1def
vhristodulo <vhristodulo@localhost>
2015-05-31 18:11:59 +0200

    Home Page

--- app/views/home.php
+++ app/views/home.php
@@ -1,161 +1,127 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
 
-<head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <title>AgeFrame - PHP MVC Framework</title>
 
-    <title>AgeFrame - PHP MVC Framework</title>
+        <link rel="stylesheet" type="text/css" href="pub/css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="pub/css/ageframe.css"/>
+        <link rel="stylesheet" type="text/css" href="pub/font-awesome/css/font-awesome.min.css"/>
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic"/>
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat:400,700"/>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="pub/css/bootstrap.min.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-    <!-- Custom CSS -->
-    <link href="pub/css/grayscale.css" rel="stylesheet">
+    </head>
 
-    <!-- Custom Fonts -->
-    <link href="pub/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand page-scroll" href="#page-top">
-                    <i class="fa fa-square-o"></i>  <span class="light">Age</span>Frame
-                </a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">About</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#download">Download</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Intro Header -->
-    <header class="intro">
-        <div class="intro-body">
+        <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <h1 class="brand-heading">AgeFrame</h1>
-                        <p class="intro-text">Simple and fast MVC framework for PHP application.<br>Written in procedural style PHP.</p>
-                        <a href="#about" class="btn btn-circle page-scroll">
-                            <i class="fa fa-angle-double-down animated"></i>
-                        </a>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top">
+                        <i class="fa fa-square-o"></i>  <span class="light">Age</span>Frame
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#about">About</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#download">Download</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <header class="intro">
+            <div class="intro-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <h1 class="brand-heading">AgeFrame</h1>
+                            <p class="intro-text">Simple and fast MVC framework for PHP application.<br>Written in procedural style PHP.</p>
+                            <a href="#about" class="btn btn-circle page-scroll">
+                                <i class="fa fa-angle-double-down animated"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-    <!-- About Section -->
-    <section id="about" class="container content-section text-center">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2">
-                <h2>About AgeFrame</h2>
-                <p>AgeFrame is a simple and fast MVC framework for PHP application. It is written in procedural style PHP. The project is open source, and you can use it for any purpose, personal or commercial.</p>
-            </div>
-        </div>
-    </section>
-
-    <!-- Download Section -->
-    <section id="download" class="content-section text-center">
-        <div class="download-section">
-            <div class="container">
+        <section id="about" class="container content-section text-center">
+            <div class="row">
                 <div class="col-lg-8 col-lg-offset-2">
-                    <h2>Download AgeFrame</h2>
-                    <p>You can download AgeFrame for free on the preview this page.</p>
-                    <a href="" class="btn btn-default btn-lg">Visit Download Page</a>
+                    <h2>About AgeFrame</h2>
+                    <p>AgeFrame is a simple and fast MVC framework for PHP application. It is written in procedural style PHP. The project is open source, and you can use it for any purpose, personal or commercial.</p>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Contact Section -->
-    <section id="contact" class="container content-section text-center">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2">
-                <h2>Contact AgeFrame Team</h2>
-                <p>Feel free to email us to provide some feedback on our framework, give us suggestions for new features and libraries, or to just say hello!</p>
-                <p><a href="mailto:feedback@ageframe.com">feedback@ageframe.com</a>
-                </p>
-                <ul class="list-inline banner-social-buttons">
-                    <li>
-                        <a href="" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
-                    </li>
-                    <li>
-                        <a href="" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                    </li>
-                    <li>
-                        <a href="" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
-                    </li>
-                </ul>
+        <section id="download" class="content-section text-center">
+            <div class="download-section">
+                <div class="container">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <h2>Download AgeFrame</h2>
+                        <p>You can download AgeFrame for free on the preview this page.</p>
+                        <a href="" class="btn btn-default btn-lg">Visit Download Page</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Map Section -->
-    <!--div id="map"></div-->
+        <section id="contact" class="container content-section text-center">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Contact AgeFrame Team</h2>
+                    <p>Feel free to email us to provide some feedback on our framework, give us suggestions for new features and libraries, or to just say hello!</p>
+                    <p><a href="mailto:feedback@ageframe.com">feedback@ageframe.com</a>
+                    </p>
+                    <ul class="list-inline banner-social-buttons">
+                        <li>
+                            <a href="" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        </li>
+                        <li>
+                            <a href="" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                        </li>
+                        <li>
+                            <a href="" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
 
-    <!-- Footer -->
-    <footer>
-        <div class="container text-center">
-            <p>Copyright &copy; AgeFrame Team 2015.</p>
-        </div>
-    </footer>
+        <footer>
+            <div class="container text-center">
+                <p>Copyright &copy; AgeFrame Team 2015.</p>
+            </div>
+        </footer>
 
-    <!-- jQuery -->
-    <script src="pub/js/jquery.js"></script>
+        <script src="pub/js/jquery.js"></script>
+        <script src="pub/js/bootstrap.min.js"></script>
+        <script src="pub/js/jquery.easing.min.js"></script>
+        <script src="pub/js/ageframe.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="pub/js/bootstrap.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="pub/js/jquery.easing.min.js"></script>
-
-    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="pub/js/grayscale.js"></script>
-
-</body>
+    </body>
 
 </html>
