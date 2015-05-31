6c74e97c27595529ee75be6f34d12682bba9e6f5
vhristodulo <vhristodulo@localhost>
2015-05-31 04:17:27 +0200

    Base Model

--- core/models/base.php
+++ core/models/base.php
@@ -1,7 +1,24 @@
 <?php
 
+    /**
+     * Base Model
+     * 
+     */
+
+    /**
+     * Define connection
+     */
     $conn = NULL;
 
+    /**
+     * Connect to database server
+     * 
+     * @global string $db_hostname
+     * @global string $db_username
+     * @global string $db_password
+     * @global string $db_database
+     * @global object $conn
+     */
     function db_connect() {
         global $db_hostname;
         global $db_username;
@@ -12,11 +29,23 @@
         $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
     }
     
+    /**
+     * Disconnect from database server
+     * 
+     * @global object $conn
+     */
     function db_disconnect() {
         global $conn;
         mysqli_close($conn);
     }
     
+    /**
+     * Execute query and fetch result into array
+     * 
+     * @global object $conn
+     * @param string $query
+     * @return array
+     */
     function db_query($query) {
         db_connect();
         global $conn;
